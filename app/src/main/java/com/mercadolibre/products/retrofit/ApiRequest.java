8f769f5254f2b3932604c0518a8deeec4bb6de96
Pablovsky <pablolopez.tr@gmail.com>
2021-02-03 20:21:51 -0300

    Revert "auto_suggest"
    This reverts commit 379ca6036e77d0cb8510900cc04909dd74216a49.

--- app/src/main/java/com/mercadolibre/products/retrofit/ApiRequest.java
+++ app/src/main/java/com/mercadolibre/products/retrofit/ApiRequest.java
@@ -1,7 +1,6 @@
 package com.mercadolibre.products.retrofit;
 
 import com.mercadolibre.products.models.search.Search;
-import com.mercadolibre.products.models.suggest.Suggest;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -11,9 +10,5 @@ public interface ApiRequest {
 
     @GET("sites/MLA/search")
     Call<Search> getItems(@Query("q") String query, @Query("offset") Integer offset, @Query("limit") Integer limit);
-
-    @GET("sites/MLA/autosuggest")
-    Call<Suggest> getSuggest(@Query("q") String query, @Query("limit") Integer limit,@Query("showFilters") Boolean showFilters);
-
 
 }
--- app/src/main/java/com/mercadolibre/products/ui/SearchDialog.java
+++ app/src/main/java/com/mercadolibre/products/ui/SearchDialog.java
@@ -2,36 +2,22 @@ package com.mercadolibre.products.ui;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.mercadolibre.products.R;
-import com.mercadolibre.products.models.suggest.SuggestQueries;
-import com.mercadolibre.products.viewmodels.ViewModelSearch;
-
-import java.util.ArrayList;
 
 public class SearchDialog extends DialogFragment {
 
     private String productDescription;
     private NoticeSearchDialog listener;
     private View myView;
-    private ViewModelSearch viewModel;
-    private ListView listView;
-    private SearchView searchView;
 
     public interface NoticeSearchDialog{
         void onDialogSearch(String productDescription);
@@ -64,30 +50,15 @@ public class SearchDialog extends DialogFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        this.viewModel= new ViewModelProvider(this).get(ViewModelSearch.class);
-        this.listView = view.findViewById(R.id.listview);
-        this.searchView = view.findViewById(R.id.search);
-        viewModel.getSuggest().observe(getViewLifecycleOwner(), suggestQueries -> {
-            ArrayList<String> suggest = new ArrayList<>();
-            for (int i = 0 ; i < suggestQueries.size() ; i++){
-                suggest.add(suggestQueries.get(i).getQ());
-            }
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, suggest);
-
-            listView.setOnItemClickListener((parent, view1, position, id) -> searchView.setQuery(suggest.get(position),false));
-
-            listView.setAdapter(adapter);
-
-
-        });
 
         if (this.productDescription != null && !this.productDescription.isEmpty()){
-            searchView.setQuery(this.productDescription,false);
+            ((android.widget.SearchView)view.findViewById(R.id.search)).setQuery(this.productDescription,false);
         }
 
         ((android.widget.SearchView)view.findViewById(R.id.search)).setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
+                //Envia
                 listener.onDialogSearch(query);
                 dismiss();
                 return false;
@@ -95,7 +66,8 @@ public class SearchDialog extends DialogFragment {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                viewModel.searchSuggest(newText);
+                //Escribe
+
                 return false;
             }
         });

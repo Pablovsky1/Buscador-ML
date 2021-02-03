022cf3fda00fdaa05cda7549c51fa022f32bd2de
Pablovsky <pablolopez.tr@gmail.com>
2021-02-03 20:22:08 -0300

    Revert "Revert "auto_suggest""
    This reverts commit 8f769f5254f2b3932604c0518a8deeec4bb6de96.

--- app/src/main/java/com/mercadolibre/products/retrofit/ApiRequest.java
+++ app/src/main/java/com/mercadolibre/products/retrofit/ApiRequest.java
@@ -1,6 +1,7 @@
 package com.mercadolibre.products.retrofit;
 
 import com.mercadolibre.products.models.search.Search;
+import com.mercadolibre.products.models.suggest.Suggest;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -10,5 +11,9 @@ public interface ApiRequest {
 
     @GET("sites/MLA/search")
     Call<Search> getItems(@Query("q") String query, @Query("offset") Integer offset, @Query("limit") Integer limit);
+
+    @GET("sites/MLA/autosuggest")
+    Call<Suggest> getSuggest(@Query("q") String query, @Query("limit") Integer limit,@Query("showFilters") Boolean showFilters);
+
 
 }
